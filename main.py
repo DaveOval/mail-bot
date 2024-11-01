026eb962b82bd6de6739f9c44331b358c5b26a0a
David Vazquez <dave_u@outlook.com>
2024-10-31 19:59:57 -0600

    mas funciones para el correo

--- main.py
+++ main.py
@@ -9,5 +9,21 @@ email_reciever = "dave_u@outlook.com"
 asunto = "Mira esta notificacion"
 
 cuerpo = '''
-    
+   Hola que pedo 
 '''
+
+email = EmailMessage()
+
+email["From"] = email_sender
+
+email["To"] = email_reciever
+
+email["Subject"] = cuerpo
+
+email.set_content(cuerpo)
+
+contexto = ssl.create_default_context()
+
+with smtplib.SMTP_SSL("smtp.gmail.com", context = contexto) as smtp:
+    smtp.login( email_sender, email_sender_password )
+    smtp.sendmail(email_sender, email_reciever, email.as_string())
