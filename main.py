132c74c5bdce96e93ed128c748e91bcdf3e18d95
David Vazquez <dave_u@outlook.com>
2024-10-31 19:08:44 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from email.message import EmailMessage
+import ssl
+import smtplib
+
+email_sender = "mailbotj@gmail.com"
+email_sender_password = "lgizhcbskstvgjjj"
+email_reciever = "dave_u@outlook.com"
+
+asunto = "Mira esta notificacion"
+
+cuerpo = '''
+    
+'''
